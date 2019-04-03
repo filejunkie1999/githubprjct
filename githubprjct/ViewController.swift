f3cbb8ad73431c5d8f2ebe12399bd117b2237a68
Aidan Ragasa <aragasa2022@208-engineering-12.local>
2019-04-03 10:46:06 -0700

    Initial Commit

---
+++ githubprjct/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  githubprjct
+//
+//  Created by Aidan Ragasa on 4/3/19.
+//  Copyright © 2019 Aidan Ragasa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
